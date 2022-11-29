f9060a178b28112ee3e321cac11c7d4d194ec669
Bruna Zunzarren <104565697+Brunazunzarrenn@users.noreply.github.com>
2022-11-29 19:15:28 -0300

    Update Ativ5.java

--- revisaoRafa/Ativ5.java
+++ revisaoRafa/Ativ5.java
@@ -1,31 +1,11 @@
-package teste;
-import java.util.Scanner;
+package br.com.revisao;
+
+import javax.swing.JOptionPane;
+
 public class Ativ5 {
+    public static void main(String[] args) {
+       
 
-	    public static void main(String[] args) {
-	        Scanner ler = new Scanner (System.in);
-	    
-	        String desc [] = new String [5];
-	        Double preco [] = new Double [5];
+    }
 
-	        for (int i = 0; i < preco.length; i++) {
-	            System.out.println("Entre com a descrição do produto "+i+":");
-	                desc[i] = ler.nextLine();
-	            System.out.println("Entre com a valor do produto "+i+":");
-	                preco[i] = ler.nextDouble()*1.1;
-	                
-	            
-	            ler = new Scanner (System.in);
-	        }
-
-	        for (int i = 0; i < preco.length; i++) {
-	            System.out.println("Produto "+ i);
-	            System.out.println("Desc : "+ desc[i]);
-	            System.out.println("Valor : "+ preco[i]);
-	            System.out.println();
-	        }
-
-	    
-	    }
-	
 }
